9430329a5e0e56cb1d3b282898e15af4f1b04667
Lavanya <lavanya.puchakayla@gmail.com>
2014-06-26 15:10:33 -0700

    Show in model data in index,show views

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,8 +1,10 @@
 class ArticlesController < ApplicationController
   def index
+  	@articles = Article.order(updated_at: :desc).limit(25)
   end
   
   def show
+  	@article = Article.find(params[:id])
   end
 
   def new
--- db/seeds.rb
+++ db/seeds.rb
@@ -2,19 +2,15 @@
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 
 # Create 8 seed categories
-categories = Category.create([
-		{ name: 'History'}, {name: 'Biology'}, {name: 'Literature'},
-		{ name: 'Mathematics'}, { name: 'Music Theory'}, { name: 'Computer Science'},
-		{ name: 'Sociology'}, {name: 'Chemistry'}
-	])
+categories = Category.create([{name: 'History'},{name: 'Biology'},{name: 'Literature'},{name: 'Mathematics'},{name: 'Music Theory'},{name: 'Computer Science'},{name: 'Sociology'},{name: 'Chemistry'}])
 
 # create 50 articles, with random titles, 250 words of content, and
 # randomly assign one of the categories above to each article
 for i in 0..49
 	title = Faker::Lorem.sentence(rand(2..10)).chomp('.')
 	content = Faker::Lorem.paragraph(word_count=250)
-
 	# randomly assign one of the categories we just created 
-	category = Category.first(offset: rand(Category.count)) 
-	a = Article.create(title: title, content: content, categories: [category,])
+	#category = Category.first(offset: rand(Category.count)) 
+	category = Category.order("RANDOM()").first
+	a = Article.create(title: title,content: content,categories: [category,])
 end
