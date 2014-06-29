10c4c565f182cbc6feb1d4acba0f17c839845054
Lavanya <lavanya.puchakayla@gmail.com>
2014-06-29 13:59:28 -0700

    Added Categoriesconroller and provided index and show views for categories

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
   root 'welcome#index'
   resources :articles
+  resources :categories
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
